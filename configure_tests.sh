5992e1da7aaca41264bc298cda036c506ca589f0
Alan Zimmerman <alan.zimm@gmail.com>
2016-01-18 22:49:37 +0200

    Kick travis

--- configure_tests.sh
+++ configure_tests.sh
@@ -3,10 +3,11 @@
 # This script is temporary until ghc-mod is fixed to reconfigure on change
 
 # Make sure the tests run against the newly installed environment
-(cd test/testdata && cabal clean && cabal configure)
+(cd test/testdata              && cabal clean && cabal install --dependencies-only && cabal configure)
 
-(cd test/testdata/cabal/cabal1 && cabal clean && cabal configure)
-(cd test/testdata/cabal/cabal2 && cabal clean && cabal configure)
-(cd test/testdata/cabal/cabal3 && cabal clean && cabal configure)
-(cd test/testdata/cabal/cabal4 && cabal clean && cabal configure)
-(cd test/testdata/cabal/foo    && cabal clean && cabal configure)
+(cd test/testdata/cabal/cabal1 && cabal clean && cabal install --dependencies-only && cabal configure)
+(cd test/testdata/cabal/cabal2 && cabal clean && cabal install --dependencies-only && cabal configure)
+(cd test/testdata/cabal/cabal3 && cabal clean && cabal install --dependencies-only && cabal configure)
+(cd test/testdata/cabal/cabal4 && cabal clean && cabal install --dependencies-only && cabal configure)
+(cd test/testdata/cabal/foo    && cabal clean && cabal install --dependencies-only && cabal configure)
+
