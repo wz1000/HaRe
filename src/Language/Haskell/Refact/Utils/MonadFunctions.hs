eac9b7ed7cc47e8c8889beb3197422917ca71c43
Alan Zimmerman <alan.zimm@gmail.com>
2017-06-19 23:26:55 +0200

    Set the IORef once, so it can be cached in ghc-mod

--- src/Language/Haskell/Refact/Utils/MonadFunctions.hs
+++ src/Language/Haskell/Refact/Utils/MonadFunctions.hs
@@ -50,6 +50,10 @@ module Language.Haskell.Refact.Utils.MonadFunctions
        -- * New ghc-exactprint interfacing
        , liftT
 
+       -- * For internal use
+       , getHookIORef
+       , setHookIORef
+
        -- * State flags for managing generic traversals
        , getRefactDone
        , setRefactDone
@@ -79,6 +83,7 @@ module Language.Haskell.Refact.Utils.MonadFunctions
        ) where
 
 import Control.Monad.State
+import Data.IORef
 import Data.List
 
 import qualified GHC           as GHC
@@ -377,6 +382,16 @@ addToNameMap ss n = do
           nm' = Map.insert ss n nm
           mtm' = Just tm { rsNameMap = nm'}
       put s { rsModule = mtm'}
+
+-- ---------------------------------------------------------------------
+
+getHookIORef :: RefactGhc (Maybe (IORef HookIORefData))
+getHookIORef = gets rsHookIORef
+
+setHookIORef :: Maybe (IORef HookIORefData) -> RefactGhc ()
+setHookIORef mref = do
+  st <- get
+  put $ st { rsHookIORef = mref }
 
 -- ---------------------------------------------------------------------
 
