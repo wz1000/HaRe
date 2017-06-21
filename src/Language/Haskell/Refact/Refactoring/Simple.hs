d5f9e13955f35d237b93a756e364a80a19736edc
Alan Zimmerman <alan.zimm@gmail.com>
2017-06-21 20:10:56 +0200

    Ask ghc-mod to set Opt_ForceRecomp on target to be loaded.
    To force the hook to be activated.

--- src/Language/Haskell/Refact/Refactoring/Simple.hs
+++ src/Language/Haskell/Refact/Refactoring/Simple.hs
@@ -34,7 +34,6 @@ pattern HsPar l s = GHC.L l (GHC.HsPar s)
 
 removeBracketTransform  :: FilePath -> SimpPos -> SimpPos -> RefactGhc ()
 removeBracketTransform fileName beginPos endPos = do
-       parseSourceFileGhc fileName
        parsed <- getRefactParsed
        let expr :: GHC.Located (GHC.HsExpr GHC.RdrName)
            expr = fromJust $ locToExp beginPos endPos parsed
