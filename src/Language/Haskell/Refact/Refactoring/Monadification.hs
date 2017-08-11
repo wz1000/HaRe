08e994ed5f9248a616b6626d0fa33645058e85ca
Stephen Adams <sa597@kent.ac.uk>
2017-08-11 16:18:31 +0100

    Added an simpler first test case

--- src/Language/Haskell/Refact/Refactoring/Monadification.hs
+++ src/Language/Haskell/Refact/Refactoring/Monadification.hs
@@ -7,6 +7,7 @@ import System.Directory (canonicalizePath)
 import qualified GHC.SYB.Utils as SYB
 import Data.Generics as SYB
 import qualified GHC as GHC
+import Control.Monad.State
 
 monadification :: RefactSettings -> GM.Options -> FilePath -> IO [FilePath]
 monadification settings cradle fileName = do
@@ -26,9 +27,11 @@ compMonadification fileName = do
 doMonadification :: FilePath -> RefactGhc ()
 doMonadification fileName = do
   parsed <- getRefactParsed
-  let (Just lrdr) = locToRdrName (15,1) parsed
-      (Just funBind) = getHsBind (GHC.unLoc lrdr) parsed
-  logData "doMonadification" funBind
+  let (Just fRdr) = locToRdrName (4,1) parsed
+      (Just fFunBind) = getHsBind (GHC.unLoc fRdr) parsed
+      (Just hRdr) = locToRdrName (11,1) parsed
+      (Just hFunBind) = getHsBind (GHC.unLoc hRdr) parsed
+  logData "doMonadification" fFunBind
   return ()
 
 
@@ -42,9 +45,50 @@ modMatchRHSExpr fun = comp
         comp' :: ParsedGRHS -> RefactGhc ParsedGRHS
         comp' (GHC.GRHS lst body) = (fun body) >>= (\newBody -> return (GHC.GRHS lst newBody))
 
-
-isFunCall :: GHC.Name -> GHC.HsExpr GHC.Name -> Boolean
-isFunCall nm (GHC.HsVar nm2) = nm == nm2
-isFunCall nm (GHC.HsPar (GHC.L _ (GHC.HsVar nm2))) = nm == nm2
-isFunCall nm (GHC.HsApp lhs _) = isFunCall nm lhs
+isFunCall :: [GHC.Name] -> GHC.HsExpr GHC.Name -> Bool
+isFunCall nms (GHC.HsVar nm2) = elem nm2 nms
+isFunCall nms (GHC.HsPar (GHC.L _ (GHC.HsVar nm2))) = elem nm2 nms
+isFunCall nms (GHC.HsApp lhs _) = isFunCall nms (GHC.unLoc lhs)
 isFunCall _ _ = False
+
+data NameState = NS {
+  num :: Int,
+  name :: String
+                    }
+
+initNS :: String -> NameState
+initNS nm = NS 1 nm
+
+getNextName :: NameState -> (GHC.RdrName, NameState)
+getNextName ns = (rdr, newState)
+  where nameStr = (name ns) ++ (show (num ns))
+        rdr = mkRdrName nameStr
+        newState = ns {num = (num ns) + 1}
+
+newName :: MonadifyState GHC.RdrName
+newName = state (\s -> let n = names s
+                           (rdr,ns) = getNextName n in
+                         (rdr, s {names = ns}))
+
+data MS = MS {
+  names :: NameState,
+  queue :: [(Maybe GHC.RdrName, ParsedLExpr)],
+  funNames :: [GHC.Name]
+                     }
+
+initMS :: [GHC.Name] -> MS
+initMS fns = let iNS = initNS "hare" in
+  MS iNS [] fns
+
+type MonadifyState = StateT MS RefactGhc
+
+popQueue :: MonadifyState (Maybe GHC.RdrName, ParsedLExpr)
+popQueue = state (\s -> let (e:es) = queue s
+                   in (e, s {queue = es}))
+
+pushQueue :: (Maybe GHC.RdrName, ParsedLExpr) -> MonadifyState ()
+pushQueue e = state (\s -> let oldQ = queue s in
+                        ((), s {queue = oldQ ++ [e]}))
+
+monadifyExpr :: [GHC.Name] -> ParsedLExpr -> RefactGhc ParsedLExpr
+monadifyExpr funNames e = undefined
--- test/testdata/Monadification/M1.hs
+++ test/testdata/Monadification/M1.hs
@@ -1,26 +1,11 @@
 module M1 where
-import Data.Char
 
-data Expr = Var Char
-          | N Int
-          | Add Expr Expr
-          | Sub Expr Expr
-          | Assign Char Expr
-          deriving Show
+f :: [a] -> b -> b
+f [] y = y
+f x y = f (g x) (h y)
 
-type Env = [(Char,Int)]
+g :: [a] -> [a]
+g x = tail x
 
-eval :: Expr -> Env -> (Int,Env)
-
-eval (Var v) env = (head [val | (x,val) <- env, x==v], env)
-
-eval (N n) env = (n,env)
-
-eval (Add e1 e2) env = let (v1,env1) = eval e1 env
-                           (v2,env2) = eval e2 env1 in
-  (v1+v2,env2)
-eval (Sub e1 e2) env = let (v1,env1) = eval e1 env
-                           (v2,env2) = eval e2 env1 in
-  (v1-v2,env2)
-eval (Assign x e) env = let (v,env1) = eval e env in
-  (v, (x,v):env1)
+h :: a -> a
+h x = id x 
