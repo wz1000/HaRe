778533b8a015f8031610dd29648c300d40032c23
Stephen Adams <sa597@kent.ac.uk>
2017-08-12 13:54:38 +0100

    Outlined the monadification process

--- src/Language/Haskell/Refact/Refactoring/Monadification.hs
+++ src/Language/Haskell/Refact/Refactoring/Monadification.hs
@@ -1,3 +1,4 @@
+{-#LANGUAGE ScopedTypeVariables #-}
 module Language.Haskell.Refact.Refactoring.Monadification
   (monadification,compMonadification) where
 
@@ -51,6 +52,8 @@ isFunCall nms (GHC.HsPar (GHC.L _ (GHC.HsVar nm2))) = elem nm2 nms
 isFunCall nms (GHC.HsApp lhs _) = isFunCall nms (GHC.unLoc lhs)
 isFunCall _ _ = False
 
+
+
 data NameState = NS {
   num :: Int,
   name :: String
@@ -86,9 +89,52 @@ popQueue :: MonadifyState (Maybe GHC.RdrName, ParsedLExpr)
 popQueue = state (\s -> let (e:es) = queue s
                    in (e, s {queue = es}))
 
+--TODO the queue should eventually become [(pattern,expr)] because when
+--the refactoring handles lets we'll want the argument to the lambda to
+-- be the variables boind by the let
+
+--TODO: Not sure if the queue should be a Maybe because I don't think there is a
+--case where a pure function would be monadified with the >> operator
 pushQueue :: (Maybe GHC.RdrName, ParsedLExpr) -> MonadifyState ()
 pushQueue e = state (\s -> let oldQ = queue s in
                         ((), s {queue = oldQ ++ [e]}))
 
-monadifyExpr :: [GHC.Name] -> ParsedLExpr -> RefactGhc ParsedLExpr
-monadifyExpr funNames e = undefined
+monadifyFunRHS :: [GHC.Name] -> ParsedLExpr -> RefactGhc ParsedLExpr
+monadifyFunRHS fNames e = let initState = initMS fNames in
+                              evalStateT (monExp e) initState 
+  where
+    --This function handles the top expression from the rhs of a function
+    monExp :: ParsedLExpr -> MonadifyState ParsedLExpr
+    monExp expr = do
+      st <- get
+      strippedExpr <- applyAtArgSubTrees stripMonArgs expr
+      (grp, _ , _ , _) <- lift getRefactRenamed
+      let (strtPos,endPos) = getStartEndLoc expr
+          (mRenExpr :: Maybe (GHC.LHsExpr GHC.Name)) = locToExp strtPos endPos (GHC.hs_valds grp)
+      renExpr <- case mRenExpr of
+        (Just e) -> return e
+        Nothing  -> error "Unable to retrieve renamed version of the expression"
+      newE <- if isFunCall (funNames st) renExpr
+              -- In this case the expression is monadic and doesn't need to be wrapped in a return
+              then
+                return strippedExpr
+              -- Otherwise the expression needs to be wrapped with a call to return
+              else
+                wrapInReturn strippedExpr
+      composeWithBinds newE
+
+--Takes the expressions from the queue and binds those expressions
+--with a lambda expression of the given 
+composeWithBinds :: ParsedLExpr -> MonadifyState ParsedLExpr
+composeWithBinds = undefined
+            
+--If this is called with a subtree that is a call to a monadified function
+--It stores the original expression in the queue and returns a HsVar with the new name
+stripMonArgs :: ParsedLExpr -> MonadifyState ParsedLExpr
+stripMonArgs = undefined
+
+
+--Takes in an expression that should represent either a function application or an operator applicator
+--The function will be applied to each argument subtree
+applyAtArgSubTrees :: (ParsedLExpr -> MonadifyState ParsedLExpr) -> ParsedLExpr -> MonadifyState ParsedLExpr
+applyAtArgSubTrees = undefined
