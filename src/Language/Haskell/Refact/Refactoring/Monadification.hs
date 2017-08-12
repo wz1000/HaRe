9c299a4e39ac5c5ea9c75239b897ba796c12838e
Stephen Adams <sa597@kent.ac.uk>
2017-08-12 17:00:45 +0100

    First try at the extraction of monadic expressions into the queue.

--- src/Language/Haskell/Refact/Refactoring/Monadification.hs
+++ src/Language/Haskell/Refact/Refactoring/Monadification.hs
@@ -52,6 +52,11 @@ isFunCall nms (GHC.HsPar (GHC.L _ (GHC.HsVar nm2))) = elem nm2 nms
 isFunCall nms (GHC.HsApp lhs _) = isFunCall nms (GHC.unLoc lhs)
 isFunCall _ _ = False
 
+isModFunCall :: ParsedLExpr -> MonadifyState Bool
+isModFunCall e = do
+  renE <- lift $ lookupRenamedExpr e 
+  st <- get
+  return $ isFunCall (funNames st) (GHC.unLoc renE)
 
 
 data NameState = NS {
@@ -96,32 +101,42 @@ popQueue = state (\s -> let (e:es) = queue s
 --TODO: Not sure if the queue should be a Maybe because I don't think there is a
 --case where a pure function would be monadified with the >> operator
 pushQueue :: (Maybe GHC.RdrName, ParsedLExpr) -> MonadifyState ()
-pushQueue e = state (\s -> let oldQ = queue s in
-                        ((), s {queue = oldQ ++ [e]}))
+pushQueue e = appendToQueue [e]
+
+appendToQueue :: [(Maybe GHC.RdrName, ParsedLExpr)] -> MonadifyState ()
+appendToQueue lst = state (\s -> let oldQ = queue s in
+                              ((), s {queue = oldQ ++ lst}))
 
 monadifyFunRHS :: [GHC.Name] -> ParsedLExpr -> RefactGhc ParsedLExpr
 monadifyFunRHS fNames e = let initState = initMS fNames in
-                              evalStateT (monExp e) initState 
-  where
+                              evalStateT (monadifyExpr e) initState 
+
     --This function handles the top expression from the rhs of a function
-    monExp :: ParsedLExpr -> MonadifyState ParsedLExpr
-    monExp expr = do
-      st <- get
-      strippedExpr <- applyAtArgSubTrees stripMonArgs expr
-      (grp, _ , _ , _) <- lift getRefactRenamed
-      let (strtPos,endPos) = getStartEndLoc expr
-          (mRenExpr :: Maybe (GHC.LHsExpr GHC.Name)) = locToExp strtPos endPos (GHC.hs_valds grp)
-      renExpr <- case mRenExpr of
-        (Just e) -> return e
-        Nothing  -> error "Unable to retrieve renamed version of the expression"
-      newE <- if isFunCall (funNames st) renExpr
-              -- In this case the expression is monadic and doesn't need to be wrapped in a return
-              then
-                return strippedExpr
-              -- Otherwise the expression needs to be wrapped with a call to return
-              else
-                wrapInReturn strippedExpr
-      composeWithBinds newE
+monadifyExpr :: ParsedLExpr -> MonadifyState ParsedLExpr
+monadifyExpr expr = do
+  st <- get
+  strippedExpr <- applyAtArgSubTrees stripMonArgs expr
+  isMonadCall <- isModFunCall expr
+  newE <- if isMonadCall
+    -- In this case the expression is monadic and doesn't need to be wrapped in a return
+          then
+            return strippedExpr
+    -- Otherwise the expression needs to be wrapped with a call to return
+          else
+            lift $ wrapWithReturn strippedExpr
+  composeWithBinds newE
+
+lookupRenamedExpr :: ParsedLExpr -> RefactGhc (GHC.LHsExpr GHC.Name) 
+lookupRenamedExpr parsedElem = do
+  (grp,_ ,_, _) <- getRefactRenamed
+  let (strtPos,endPos) = getStartEndLoc parsedElem
+      (mRenExpr :: Maybe (GHC.LHsExpr GHC.Name)) = locToExp strtPos endPos (GHC.hs_valds grp)
+  return $ gfromJust "lookupRenamedExpr: Renamed expression not found" mRenExpr
+
+--Wraps the given expression with a return call
+--If the expression isn't a HsPar it will be parenthisized first
+wrapWithReturn :: ParsedLExpr -> RefactGhc ParsedLExpr
+wrapWithReturn e = undefined
 
 --Takes the expressions from the queue and binds those expressions
 --with a lambda expression of the given 
@@ -131,10 +146,41 @@ composeWithBinds = undefined
 --If this is called with a subtree that is a call to a monadified function
 --It stores the original expression in the queue and returns a HsVar with the new name
 stripMonArgs :: ParsedLExpr -> MonadifyState ParsedLExpr
-stripMonArgs = undefined
+stripMonArgs e = do
+  st <- get
+  let oldQ = queue st
+  put (st {queue = []})
+  stripped <- applyAtArgSubTrees stripMonArgs e
+  newQ <- get >>= (\s -> return (queue s))
+  modify (\s -> s {queue = oldQ})
+  isMonadCall <- isModFunCall e
+  resE <- if isMonadCall 
+          then do
+    --If this expression is a call to monad then we need to generate a new name
+    --locate (HsVar newName)
+    --push the new name and original expression onto the queue
+    nm <- newName
+    lE <- lift (locate (GHC.HsVar nm))
+    pushQueue ((Just nm), e)
+    return lE  
+          else
+    --otherwise just return the stripped expression
+            return stripped
+  --If the whole expression is monadic then it needs to be added to the queue
+  --before the monadic calls in this expression's arguments
+  appendToQueue newQ
+  return resE
 
 
 --Takes in an expression that should represent either a function application or an operator applicator
 --The function will be applied to each argument subtree
 applyAtArgSubTrees :: (ParsedLExpr -> MonadifyState ParsedLExpr) -> ParsedLExpr -> MonadifyState ParsedLExpr
-applyAtArgSubTrees = undefined
+applyAtArgSubTrees f (GHC.L l (GHC.HsApp lhs@(GHC.L _ (GHC.HsVar _)) rhs)) = f rhs >>= (\newRhs -> return (GHC.L l (GHC.HsApp lhs newRhs)))
+applyAtArgSubTrees f (GHC.L l (GHC.HsPar e)) = do
+  newE <- applyAtArgSubTrees f e
+  return (GHC.L l (GHC.HsPar newE))
+applyAtArgSubTrees f (GHC.L l (GHC.HsApp lhs rhs)) = do
+  newLhs <- applyAtArgSubTrees f lhs
+  newRhs <- f rhs
+  return (GHC.L l (GHC.HsApp newLhs newRhs))
+applyAtArgSubTrees _ ast = return ast
